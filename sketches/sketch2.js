d20d5c9f971202d0409f9b6d354288765fee9b28
Madhumitha Sridhar <madhumithasridhar@Madhumithas-MacBook-Pro.local>
2026-02-08 10:45:32 -0800

    feat: implement linear mapping for sun horizontal position and sine arc for vertical path; goal: connect real world time to sun's movement.

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -7,13 +7,35 @@ registerSketch('sk2', function (p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.textAlign(p.CENTER, p.CENTER);;
   };
+
   p.draw = function () {
+    let h = p.hour();
+    let m = p.minute();
+    let s = p.second();
+    let totalHours = h + (m / 60) + (s / 3600);
+
     p.background(210, 240, 245);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    // p.text('HWK #4. A', p.width / 2, p.height / 2);
+    
+    let sunPos = getSunPosition(totalHours);
+    drawSun(sunPos.x, sunPos.y);
+    
     drawTimeline();
   };
+
+  function getSunPosition(time) {
+    let x = p.map(time, 0, 24, margin, p.width - margin);
+    let angle = p.map(time, 0, 24, 0, p.PI);
+    let arcHeight = 330; 
+    let y = horizonY - p.sin(angle) * arcHeight;
+    return { x, y };
+  }
+
+  function drawSun(x, y) {
+    p.noStroke();
+    p.fill(255, 204, 0);
+    p.circle(x, y, 70);
+  }
+
   function drawTimeline() {
     p.noStroke();
     p.fill(185, 225, 135);
@@ -35,5 +57,6 @@ registerSketch('sk2', function (p) {
       p.text(label, x, horizonY + 35);
     });
   }
+  
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
