3dc6e9bb673e1938661ce669fdbb016b634d5007
Madhumitha Sridhar <madhumithasridhar@Madhumithas-MacBook-Pro.local>
2026-02-08 13:50:18 -0800

    feat: adding color based background color and making it hour based; adding basic UI

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -8,7 +8,18 @@ registerSketch('sk4', function(p) {
   };
 
   p.draw = function() {
-    // Placeholder background
-    p.background(220, 30, 10); 
+    let h = p.hour();
+    let m = p.minute();
+
+    // color is based on hour progress
+    let totalProgress = (h + m / 60) / 24;
+    let currentHue = totalProgress * 360; 
+    p.background(currentHue, 25, 7, 1);
+
+    // placeholder text
+    p.fill(currentHue, 50, 90);
+    p.textSize(32);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.text(`${h}:${m}`, p.width / 2, p.height / 2);
   };
 });
